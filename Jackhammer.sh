8e13e8f1a71cd500acaa9e1cde80a3c242a54698
actuallyfro <actuallyfro@github.com>
2022-12-12 19:40:54 -0600

    {Jackhammer.sh} Adding folder empty check

--- Jackhammer.sh
+++ Jackhammer.sh
@@ -7,7 +7,7 @@ if [ ! -d Analysis_Folder ]; then
 	# Throw notice and quit!
   echo "Jackhammer Text Miner: Analytics"
   echo "================================"
-	echo "Analysis_Folder not found. Creating one for you."
+	echo "Analysis_Folder not found. Creating one for you!"
 	echo "Please place your folders, with PDF's, in the Analysis_Folder folder."
 	echo ""
 	echo "Then run this script again."
@@ -16,6 +16,17 @@ if [ ! -d Analysis_Folder ]; then
 fi
 
 cd Analysis_Folder
+
+# determine if empty, throw error
+if [ ! "$(ls -A .)" ]; then
+	echo "Jackhammer Text Miner: Analytics"
+	echo "================================"
+	echo "Analysis_Folder is empty. Please place your folders, with PDF's, in the Analysis_Folder folder."
+	echo ""
+	echo "Then run this script again."
+	exit
+fi
+
 ListOfFolders=`ls -l -d */ | grep -v include | awk '{print $9}' | tr -d "/"`
 
 for Folder in $ListOfFolders; do
